35d57cfffd353982d5d0b3cf3c064a99b07cc37e
Igor Barulin <igorbarulin@gmail.com>
2019-03-31 21:59:25 +0300

    remove old code

--- FastZip/Application/Loggers/Components/LogCategory.cs
+++
@@ -1,9 +0,0 @@
-namespace Loggers.Components
-{
-    public static class LogCategory
-    {
-        public const string Error = "Error";
-        public const string Warning = "Warning";
-        public const string Info = "Information";
-    }
-}
