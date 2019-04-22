3ca7a513ada2b9e1642978834f81180d8a164bfa
Igor Barulin <igorbarulin@gmail.com>
2019-04-22 03:10:39 +0300

    oops

--- FastZip/Testing/SystemTests/GzipCompressorTests.cs
+++ FastZip/Testing/SystemTests/GzipCompressorTests.cs
@@ -39,8 +39,7 @@ namespace SystemTests
         public void TestBasicFlow()
         {
             Assert.That(_compressor.Compress(_originalFileName, _compressedFileName).HaveError, Is.False);
-            var result = _compressor.Decompress(_compressedFileName, _decompressedFileName);
-            Assert.That(result.HaveError, Is.False);
+            Assert.That(_compressor.Decompress(_compressedFileName, _decompressedFileName).HaveError, Is.False);
         }
 
         [Test]
